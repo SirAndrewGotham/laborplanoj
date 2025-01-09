232b2de1c14ef22ca20038c6fc6bc798c8353d5a
AndrewGotham <andrewgotham@hotmail.com>
2025-01-09 13:13:42 +0300

    column and card design with tailwind and alpine

--- resources/views/frontend/boards/board-show.blade.php
+++ resources/views/frontend/boards/board-show.blade.php
@@ -4,4 +4,12 @@
             {{ $board->name }}
         </h2>
     </x-slot>
+
+    <div class="w-full p-6 overflow-x-scroll">
+        <div class="flex w-max space-x-6 h-[calc(theme('height.screen')-64px-73px-theme('padding.12'))]">
+            @foreach(range(1, random_int(1, 10)) as $column)
+                <livewire:column />
+            @endforeach
+        </div>
+    </div>
 </div>
